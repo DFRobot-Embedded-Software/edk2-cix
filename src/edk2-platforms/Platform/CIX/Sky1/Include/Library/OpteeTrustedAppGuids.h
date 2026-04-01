3f61989855ebd3a6926c57dd1ad43de8305684e9
lr <1@qq.com>
2026-04-01 16:08:54 +0800

    fix: convert edk2 directories to submodules

--- src/edk2-platforms/Platform/CIX/Sky1/Include/Library/OpteeTrustedAppGuids.h
+++
@@ -1,7 +0,0 @@
-#ifndef __OPTEE_TRUSTED_APP_GUIDS_H__
-#define __OPTEE_TRUSTED_APP_GUIDS_H__
-
-extern EFI_GUID  gOpteeHelloWorldTaGuid;
-extern EFI_GUID  gOpteeFtpmTaGuid;
-
-#endif // __OPTEE_TRUSTED_APP_GUIDS_H__
--- src/edk2-platforms/Platform/DFRobot/Platforms/CIX/Sky1/Include/DFRobotSetupVar.h
+++
@@ -1,6 +0,0 @@
-#ifndef _DFROBOT_SETUP_VAR_H_
-#define _DFROBOT_SETUP_VAR_H_
-
-#include "RadxaSetupVar.h"
-
-#endif
